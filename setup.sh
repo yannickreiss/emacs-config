0f2c681b8ac109de8c6510dba4349539eed89315
yannickreiss <yannick.reiss@protonmail.ch>
2023-02-23 14:06:00 +0100

    update

--- setup.sh
+++ setup.sh
@@ -1,8 +1,7 @@
+mkdir ~/.emacs.d
 mkdir ~/.emacs.d/plugins 
+cp ./.emacs ~/.emacs
+mkdir ~/.emacs.d/snippet
+cp -r snippet/* ~/.emacs.d/snippet/
 cd ~/.emacs.d/plugins 
 git clone --recursive https://github.com/joaotavora/yasnippet.git
-# TODO: Add source for .emacs file here (wget)
-mkdir ~/.emacs.d/snippet
-cd ~/.emacs.d/snippet 
-# TODO: Get files from a repository
-
