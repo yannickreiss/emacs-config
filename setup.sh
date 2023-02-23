412d9a35619a897963714a676ef9bea4a83a552a
yannickreiss <yannick.reiss@protonmail.ch>
2023-02-23 14:22:26 +0100

    added files

--- setup.sh
+++ setup.sh
@@ -2,6 +2,6 @@ mkdir ~/.emacs.d
 mkdir ~/.emacs.d/plugins 
 cp ./.emacs ~/.emacs
 mkdir ~/.emacs.d/snippet
-cp -r snippet/* ~/.emacs.d/snippet/
+cp -r snippets/* ~/.emacs.d/snippet/
 cd ~/.emacs.d/plugins 
 git clone --recursive https://github.com/joaotavora/yasnippet.git
